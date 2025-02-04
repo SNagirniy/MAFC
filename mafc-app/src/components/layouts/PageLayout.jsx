30e1f44072a946a71fe3e0101b824e4c7db3b770
SNagirniy <86325813+SNagirniy@users.noreply.github.com>
2025-02-04 13:59:36 +0200

    initial

---
+++ mafc-app/src/components/layouts/PageLayout.jsx
@@ -0,0 +1,16 @@
+import Header from "../modules/header/Header";
+import Footer from "../modules/footer/Footer";
+
+const PageLayout = ({children})=> {
+    return (
+        <>
+        <Header/>
+        <main >{children}</main>
+        <Footer/>
+        </>
+        
+            )
+};
+
+
+export default PageLayout
---
+++ mafc-app/src/components/modules/header/Header.jsx
@@ -0,0 +1,12 @@
+import s from './header.module.scss';
+
+
+const Header =()=> {
+    return (
+        <header className={s.header}>
+
+        </header>
+    )
+};
+
+export default Header;
