7fa81e95aee0f9b4268f94ac57ea65e9e033acb1
SNagirniy <86325813+SNagirniy@users.noreply.github.com>
2025-06-09 22:43:47 +0300

    strapi data upload fix

--- mafc-app/src/app/entrants/calendar/page.jsx
+++ mafc-app/src/app/entrants/calendar/page.jsx
@@ -1,11 +1,11 @@
 import ApplicantsCalendar from "@/components/modules/ApplicantsCalendar/ApplicantsCalendar";
 import { getEducationalCalendar } from "@/server/strapi/strapi";
 
+export const revalidate = 3600;
+
 const Calendar = async()=> {
 
     const data = await getEducationalCalendar();
-
-    if(!data) {return <ApplicantsCalendar calendarData={[]}/>}
 
     return <ApplicantsCalendar calendarData={data}/>
 };
